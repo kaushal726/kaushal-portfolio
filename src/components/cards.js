fae07e8cee5f0e736a8882b542feaea603e079a2
Kaushal <Kausraj11@gmail.com>
2023-06-26 22:49:44 +0530

    Changes Made

--- src/components/cards.js
+++ src/components/cards.js
@@ -3,8 +3,8 @@ function Cards(props) {
     return (
         <div className="bg-slate-200 shadow-md rounded-lg p-4 ">
             <h1 className="text-xl text-center font-bold mb-2" >{props.Heading}</h1>
-            <p className="text-gray-700 mb-4" >{props.Paragraph}</p>
-            <div className="text-center"><a className="text-center" target="_blank" rel="noreferrer" href={props.href}>{props.Heading}</a></div>
+            <p className="text-gray-700 mb-4 md:text-sm text-xs h-10" >{props.Paragraph}</p>
+            <div className="text-center p-8"><a className="text-center" target="_blank" rel="noreferrer" href={props.href}>{props.Heading}</a></div>
             <img className="w-full rounded-lg" src={props.src} alt="" />
         </div>
     )
