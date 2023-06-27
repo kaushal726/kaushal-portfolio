d235f542569818e5a5eced9e1c226d03e21df1ed
Kaushal <Kausraj11@gmail.com>
2023-06-28 01:00:56 +0530

    changes made

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Navbar() {
     return (
         <div>
-            <nav className='p-3 flex items-center justify-center  text-white md:py-4 py-2'>
-                <div className='tablet:text-neutral-100'>
+            <nav className='p-3 flex items-center justify-center  text-white md:py-4 py-3'>
+                <div className='tablet:text-neutral-100  border-b-2 border-grey md:pb-6 md:w-96  flex items-center justify-center pb-3 w-72'>
                     <a className='px-3 md:text-base text-xs text-white font-semibold' href='/#'>Home</a>
                     <a className='px-3 md:text-base text-xs text-white font-semibold' href='/#'>About</a>
                     <a className='px-3 md:text-base text-xs text-white font-semibold' href='/#'>Projects</a>
