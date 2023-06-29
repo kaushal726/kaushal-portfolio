cdcadaebba397a144b663b806c0661e90f4c55b7
Kaushal <Kausraj11@gmail.com>
2023-06-30 00:23:08 +0530

    changes made

--- src/components/body.js
+++ src/components/body.js
@@ -8,7 +8,7 @@ export const Body = () => {
             <div className='relative flex-col top-0 flex justify-center items-start w-full md:h-96 h-40 md:pl-16 pl-9'>
                 <h1 className='md:text-5xl italic font-medium p-2 text-1xl  py-0 text-white'>Hi There,</h1>
                 <h1 className='md:text-5xl italic font-medium p-2 text-1xl  py-0 text-white'>I'm <span className='text-orange-400'>Kaushal Raj</span></h1>
-                <h1 className='md:text-5xl italic font-medium p-2 text-1xl  py-0  text-white'>Frontend Developer </h1>
+                <h1 className='md:text-5xl italic font-medium p-2 text-1xl  py-0  text-white'>Full Stack Developer </h1>
             </div>
             <div className='relative flex-col top-0 flex justify-center items-center w-full md:h-96 h-40'>
                 <img className='rounded-full md:w-96  w-36' src={displayPicture} alt=''></img>
