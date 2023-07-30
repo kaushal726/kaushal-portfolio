aba4f1ad3c65b593ba46f01face83a669ef16ebe
Kaushal <Kausraj11@gmail.com>
2023-07-30 12:15:12 +0530

    Updated package.json and changes Body

--- src/components/Home/Body.js
+++ src/components/Home/Body.js
@@ -5,10 +5,11 @@ export const Body = () => {
     return (
         <div className='top-0 flex justify-center items-center  h-screen overflow-x-auto'>
             <div className=' pb-24 relative flex-col top-0 flex justify-center items-start  md:h-96  md:pl-16 pl-9'>
-                <h1 className='md:text-5xl italic font-medium p-2 text-1xl  py-0 text-white'>Hi There,</h1>
+                {/* <h1 className='md:text-5xl italic font-medium p-2 text-1xl align-center py-0 text-white'>Hi There,</h1>
                 <h1 className='md:text-5xl italic font-medium p-2 text-1xl  py-0 text-white'>I'm <span
                     className='text-orange-400'>Kaushal Raj</span></h1>
-                <h1 className='md:text-5xl italic font-medium p-2 text-1xl  py-0  text-white'>Full Stack Developer </h1>
+                <h1 className='md:text-5xl italic font-medium p-2 text-1xl  py-0  text-white'>Full Stack Developer </h1> */}
+                <h1 className='md:text-5xl italic font-medium p-2 text-1xl  py-0  text-white'>Under Maintenance</h1>
             </div>
             {/* <div className='relative flex-col top-0 flex justify-center items-center w-full md:h-96 h-40'>
                     <img className='rounded-full md:w-96  w-36' src={displayPicture} alt=''></img>
