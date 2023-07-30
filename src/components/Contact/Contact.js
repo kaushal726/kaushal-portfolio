3430c9505c5dc54ec47eea599de19d8f8d8b9ef8
Kaushal <Kausraj11@gmail.com>
2023-07-30 12:10:43 +0530

    Updated package.json and changes Body

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -47,30 +47,33 @@ let Contact = () => {
         setDetails([])
     };
     return (
-        <div className="p-5 bg-white m-5 rounded-lg">
-            <h2 className="text-center text-3xl font-semibold italic">Contact Us</h2>
-            <form className=" grid grid-rows-3">
-                <label className="block text-gray-700 font-bold mt-2" htmlFor="name" >Name : </label>
-                <input className="w-full border border-gray-300 rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500" id="name" type="text" placeholder="Enter Your Name" onChange={updateName} value={name} />
-                <label className="block text-gray-700 font-bold mt-2" htmlFor="email" >Email : </label>
-                <input className="w-full border border-gray-300 rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500" id="email" type="email" placeholder="Enter Your Email" onChange={updateEmail} value={email} />
-                <label className="block text-gray-700 font-bold mt-2" htmlFor="message" >Suggestions  : </label>
-                <textarea className="w-full border border-gray-300 rounded py-2 px-3 text-gray-700 leading-tight resize-none h-24 focus:outline-none focus:border-blue-500" id="message" name="w3review" rows="6" cols="60" onChange={updateSummary} value={summary}></textarea>
-                <button className=" bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 mt-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" onClick={upload}>Submit</button>
-            </form>
-            {popUp && (
-                <div className="z-40 fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-black bg-opacity-50">
-                    <div className="flex flex-col bg-white p-4 rounded-lg">
-                        <div className="flex items-end justify-end">
-                            <button className="mb-4 text-gray-600 hover:text-gray-900" onClick={handleDismiss}>
-                                <i className="fa-solid fa-xmark fa-beat"></i>
-                            </button>
+        <div className='h-screen'>
+            <div className=" p-5 bg-white m-5 rounded-lg">
+                <h2 className="text-center text-3xl font-semibold italic">Contact Us</h2>
+                <form className=" grid grid-rows-3">
+                    <label className="block text-gray-700 font-bold mt-2" htmlFor="name" >Name : </label>
+                    <input className="w-full border border-gray-300 rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500" id="name" type="text" placeholder="Enter Your Name" onChange={updateName} value={name} />
+                    <label className="block text-gray-700 font-bold mt-2" htmlFor="email" >Email : </label>
+                    <input className="w-full border border-gray-300 rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500" id="email" type="email" placeholder="Enter Your Email" onChange={updateEmail} value={email} />
+                    <label className="block text-gray-700 font-bold mt-2" htmlFor="message" >Suggestions  : </label>
+                    <textarea className="w-full border border-gray-300 rounded py-2 px-3 text-gray-700 leading-tight resize-none h-24 focus:outline-none focus:border-blue-500" id="message" name="w3review" rows="6" cols="60" onChange={updateSummary} value={summary}></textarea>
+                    <button className=" bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 mt-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" onClick={upload}>Submit</button>
+                </form>
+                {popUp && (
+                    <div className="z-40 fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-black bg-opacity-50">
+                        <div className="flex flex-col bg-white p-4 rounded-lg">
+                            <div className="flex items-end justify-end">
+                                <button className="mb-4 text-gray-600 hover:text-gray-900" onClick={handleDismiss}>
+                                    <i className="fa-solid fa-xmark fa-beat"></i>
+                                </button>
+                            </div>
+                            <h2 className="text-2xl italic font-bold mb-4">{message}</h2>
                         </div>
-                        <h2 className="text-2xl italic font-bold mb-4">{message}</h2>
                     </div>
-                </div>
-            )}
+                )}
+            </div>
         </div>
+
     )
 }
 export default Contact;
--- src/components/Home/Body.js
+++ src/components/Home/Body.js
@@ -1,21 +1,19 @@
 import React from 'react'
 // import image2 from '../assets/Background/bg-5.jpg'
-import displayPicture from '../../assets/dp.jpg'
-import { Element } from 'react-scroll'
+// import displayPicture from '../../assets/dp.jpg'
 export const Body = () => {
     return (
-        <Element name='home'>
-            <div className='grid grid-cols-2'>
-                <div className='relative flex-col top-0 flex justify-center items-start w-full md:h-96 h-40 md:pl-16 pl-9'>
-                    <h1 className='md:text-5xl italic font-medium p-2 text-1xl  py-0 text-white'>Hi There,</h1>
-                    <h1 className='md:text-5xl italic font-medium p-2 text-1xl  py-0 text-white'>I'm <span className='text-orange-400'>Kaushal Raj</span></h1>
-                    <h1 className='md:text-5xl italic font-medium p-2 text-1xl  py-0  text-white'>Full Stack Developer </h1>
-                </div>
-                <div className='relative flex-col top-0 flex justify-center items-center w-full md:h-96 h-40'>
-                    <img className='rounded-full md:w-96  w-36' src={displayPicture} alt=''></img>
-                </div>
+        <div className='top-0 flex justify-center items-center  h-screen overflow-x-auto'>
+            <div className=' pb-24 relative flex-col top-0 flex justify-center items-start  md:h-96  md:pl-16 pl-9'>
+                <h1 className='md:text-5xl italic font-medium p-2 text-1xl  py-0 text-white'>Hi There,</h1>
+                <h1 className='md:text-5xl italic font-medium p-2 text-1xl  py-0 text-white'>I'm <span
+                    className='text-orange-400'>Kaushal Raj</span></h1>
+                <h1 className='md:text-5xl italic font-medium p-2 text-1xl  py-0  text-white'>Full Stack Developer </h1>
             </div>
-        </Element>
+            {/* <div className='relative flex-col top-0 flex justify-center items-center w-full md:h-96 h-40'>
+                    <img className='rounded-full md:w-96  w-36' src={displayPicture} alt=''></img>
+                </div> */}
+        </div>
 
 
     )
